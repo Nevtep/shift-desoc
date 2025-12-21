a59932b62c2ca3f9d3802a2c5029509a78252fc5
Carlos Brugiafredo <brugiafredo@gmail.com>
2025-12-21 13:07:26 +0100

    fix error fixed header

--- apps/marketing/src/sections/Header.tsx
+++ apps/marketing/src/sections/Header.tsx
@@ -190,8 +190,11 @@ export default function Header() {
       {menuOpen && (
         <>
           <Stack
-            position="fixed"
-            inset={0}
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
             backgroundColor="rgba(0,0,0,0.25)"
             zIndex={1000}
             display="none"
